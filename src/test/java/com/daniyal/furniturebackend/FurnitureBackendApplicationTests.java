8dccfab3bacac87995dc6dc286d0f49ae1f658b1
DanialFS <danial20051225@gmail.com>
2025-10-31 00:40:15 +0500

    Initial commit: Spring Boot furniture backend with JPA and PostgreSQL

---
+++ src/test/java/com/daniyal/furniturebackend/FurnitureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniyal.furniturebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurnitureBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
